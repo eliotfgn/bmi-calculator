214b8bb2ac76b4a832ea3bb9650a33cbb7c46c18
Eliot Fagnon <efagnon2002@gmail.com>
2022-11-20 18:58:23 +0100

    bmi calculation result content component

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/main.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
@@ -6,24 +7,35 @@ class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Text("Your result"),
-          const ResultContent(),
-          GestureDetector(
-            child: Container(
-              color: Colors.red,
-              height: 70,
-              width: double.infinity,
-              child: const Center(
-                child: Text(
-                  "Re-Calculate",
-                  style: TextStyle(fontSize: 25),
-                ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 35),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                "Your result",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.start,
               ),
             ),
-          )
-        ],
+            ResultContent(),
+            GestureDetector(
+              child: Container(
+                color: Colors.red,
+                height: 70,
+                width: double.infinity,
+                child: Center(
+                  child: Text(
+                    "Re-Calculate".toUpperCase(),
+                    style: const TextStyle(fontSize: 25),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -34,6 +46,37 @@ class ResultContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(vertical: 40, horizontal: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        decoration: BoxDecoration(
+          color: inactiveCard,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const [
+            Text(
+              "OVERWEIGHT",
+              style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff24d876)),
+            ),
+            Text(
+              "26.5",
+              style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "You have a higher than normal body weight. Try to exercise a little bit.",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
